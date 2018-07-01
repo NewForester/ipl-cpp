c7e73373991ae60a8caae0b932993667a5196f5c
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-07-01 18:42:44 +0100

    add a solution for the Exercism hello-world exercise

---
+++ exercism/hello_world.h
@@ -0,0 +1,18 @@
+#if !defined(HELLO_WORLD_H)
+#define HELLO_WORLD_H
+
+#include <string>
+
+namespace hello_world {
+    std::string hello() {
+        return "Hello, World!";
+    }
+}
+
+#endif
+
+//
+//  This execise is just to check you have everything installed.
+//
+//  Some managed to make a meal of this.  At least two used cout.
+//
