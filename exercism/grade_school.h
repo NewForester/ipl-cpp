0259059108312f488500bae1b44dee909a6256f6
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-07-14 21:33:34 +0100

    add a revised solution for the Exercism grade school exercise

--- exercism/grade_school.h
+++ exercism/grade_school.h
@@ -5,30 +5,19 @@
 #include <map>
 #include <string>
 
-using namespace std;
-
 namespace grade_school {
+    using namespace std;
+
     class school {
     public:
         school() {}
 
         school &add(string name, int grade) {
-            auto it = students_by_grade.find(grade);
+            auto& students = students_by_grade[grade];
+            auto range = equal_range(students.begin(), students.end(), name);
 
-            if (it == students_by_grade.end()) {
-                vector<string> students(1, name);
-
-                students_by_grade.insert(pair<int, vector<string>>(grade, students));
-            } else {
-                vector<string> &students = (*it).second;
-
-                if (find(students.begin(), students.end(), name) == students.end())
-                {
-                    students.push_back(name);
-
-                    sort(students.begin(), students.end());
-                }
-            }
+            if (range.first == range.second)
+                students.insert(range.first, name);
 
             return *this;
         }
@@ -37,8 +26,8 @@ namespace grade_school {
             return students_by_grade;
         }
 
-        const vector<string> grade(int which) {
-            auto it = students_by_grade.find(which);
+        const vector<string> grade(int grade) {
+            auto it = students_by_grade.find(grade);
 
             return it == students_by_grade.end() ? vector<string>() : (*it).second;
         }
@@ -50,15 +39,9 @@ namespace grade_school {
 #endif
 
 //
-//  This solution uses a map to store students by grade.
+//  This solution uses equal_range() to avoid adding duplicates and also avoid
+//  the need to sort the vector of names over and over.  It also relies on
+//  map::[] to insert and return an empty vector first time.
 //
-//  My solution is longer than all the others, this is partly because the
-//  others use less white space and partly because they use a header and an
-//  implementation file and finally because the shorter ones will happily add
-//  the same student twice to the same grade.
-//
-//  Two solutions used sort as I did (and checked for duplicates first).  The
-//  others all used insert() with an iterator returned by one of lower_bound(),
-//  upper_bound() or equal_bound().  I suspect this last is the only way to be
-//  sure of avoiding duplicates.
+//  This result is significantly shorter and less complex.
 //
