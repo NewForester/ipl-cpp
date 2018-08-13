ee4381ae50311b66b58dd38814f1565ea8575a4b
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-08-13 17:04:32 +0100

    add a revised solution for the Exercism hexadecimal exercise

--- exercism/hexadecimal.h
+++ exercism/hexadecimal.h
@@ -1,57 +1,35 @@
 #if !defined(HEXADECIMAL_H)
 #define HEXADECIMAL_H
 
+#include <algorithm>
 #include <cctype>
 #include <string>
 #include <utility>
 
 namespace hexadecimal {
-    using couple = std::pair<int, int>;
+    using outcome = std::pair<bool, int>;
 
     const std::string hexdigits = "0123456789abcdef";
 
-    couple recurse(const char *hex) {
-        const int xigit = hexdigits.find(tolower(*hex));
+    int convert(const std::string &ss) {
+        auto result = std::accumulate(ss.begin(), ss.end(), outcome(true, 0),
+            [] (outcome sofar, char hex) {
+                const size_t xigit = hexdigits.find(tolower(hex));
 
-        if (xigit == std::string::npos)
-            return couple(0, *hex == '\0');
+                sofar.first &= (xigit != std::string::npos);
+                sofar.second = (sofar.second << 4) + xigit;
 
-        const auto result = recurse(hex + 1);
+                return sofar;
+            });
 
-        const int increment = result.second * xigit;
-
-        return couple(result.first + increment, result.second * 16);
-    }
-
-    int convert(const char *hex) {
-        recurse(hex).first;
+        return result.first ? result.second : 0;
     }
 }
 
 #endif
 
 //
-//  This solution follows the design of my binary and ternary solutions.
-//
-//  This is a job for std::accumulate and a simple lambda.  My preference is
-//  for one that uses string::find but everyone did it the hard way, even the
-//  solution that used string::find (the opposite) as validation.
-//
-//  The trouble with using std::accumulate is how to return 0 given invalid
-//  input.  Both solutions threw and caught an exception.
-//
-//  Next option is whether to have a separate validation pass.  This is ok on
-//  short strings (as here) especially if that simplifies the evaluation pass
-//  (as it should).  A couple of solutions did this well and another badly.
-//
-//  One solution used the experimental nullopt which looks closer to my return
-//  of pair<>.  These don't really help.
-//
-//  Finally, you can do both at once, in which case, you need only one loop, no
-//  subroutine and you just return if you find an invalid character.
-//
-//  Odd no one tried a validation pass followed by std::accumulate.  Also odd
-//  is that only one used isxdigit() for validation.
-//
-//  Two used math::pow but only one used << 4 to produce powers of 16.
+//  This solution drops the recursion and uses std::accumulate instead.  It
+//  uses std::pair to return an error and so avoid throwing and catching an
+//  exception.  I know, very go.
 //
