077eab4662d418d2d3c66538951925cb52d77116
Paul Bryan Roberts <NewForester@users.noreply.github.com>
2018-08-14 20:34:27 +0100

    add a revised solution for the Exercism bracket push exercise

--- exercism/bracket_push.h
+++ exercism/bracket_push.h
@@ -1,47 +1,40 @@
 #if !defined(BRACKET_PUSH_H)
 #define BRACKET_PUSH_H
 
-#include <vector>
+#include <stack>
+#include <string>
 
 namespace bracket_push {
+    const std::string brackets = "(){}[]";
+
     bool check(const char *expression) {
-        std::string stack;
+        std::stack<size_t> stack;
 
         for (auto character : std::string(expression))
-            switch (character) {
-                case '(':
-                    stack.push_back(')');
+            switch (size_t index = brackets.find(character)) {
+                case 0:
+                case 2:
+                case 4:
+                    stack.push(index + 1);
                     break;
-                case '{':
-                    stack.push_back('}');
-                    break;
-                case '[':
-                    stack.push_back(']');
-                    break;
-                case ')':
-                case '}':
-                case ']':
-                    if (stack.size() == 0 || character != stack.back())
+                case 1:
+                case 3:
+                case 5:
+                    if (stack.empty() || stack.top() != index)
                         return false;
 
-                    stack.pop_back();
+                    stack.pop();
             }
 
-        return stack.size() == 0;
+        return stack.empty();
     }
 }
 
 #endif
 
 //
-//  This solution uses a switch and makes the pop common.
+//  This solution uses std::stack instead of std::vector.
 //
-//  Well, all but one of the solutions examined where over complicated or long
-//  winded suggesting folk started coding and end up digging themselves out of
-//  a hole.  Perhaps the problem sounds easier than it is.
+//  It still uses a switch but now has two simple cases.  It is not, in terms
+//  of lines, any shorter that the previous solution.
 //
-//  Almost all used std::stack but this did not help.  std::string or
-//  std::vector are perfectly adequate.
-//
-
-
